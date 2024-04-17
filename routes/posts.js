ed93399716a25f165b0b1ba87d806d6446ecd158
Roberto Payan <rpayan@acedisposal.com>
2024-04-17 10:57:24 -0600

    Se formatea el post.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,17 @@ Route: /api/posts
 */
 
 const { Router } = require("express");
-const expressfileUpload = require('express-fileupload');
+const expressfileUpload = require("express-fileupload");
 
-
-const {check} = require('express-validator');
-const {validateFields} = require('../middlewares/validate-fields')
-const { getPosts, createPost, updatePost, deletePost, getImage } = require("../controllers/posts");
+const { check } = require("express-validator");
+const { validateFields } = require("../middlewares/validate-fields");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  getImage,
+} = require("../controllers/posts");
 const validateJwt = require("../middlewares/validate-jwt");
 const router = Router();
 
@@ -17,24 +22,24 @@ const router = Router();
 //     limits: { fileSize: 1 * 1024 * 1024 },
 //   }));
 
-  router.use(expressfileUpload());
+router.use(expressfileUpload());
 
 //router.get('/', validateJwt, getPosts);
-router.get('/', [], getPosts); // En este caso no validaremos el jwt
+router.get("/", [], getPosts); // En este caso no validaremos el jwt
 
+router.get("/image/:image", [], getImage);
 
-router.get('/image/:image', [], getImage);
-
-router.post('/',[
-
+router.post(
+  "/",
+  [
     validateJwt,
 
-  //  check('name', 'The name is required').notEmpty(),
-   // check('image', 'The image is required').notEmpty(),
-    check('price', 'The price is required').notEmpty(),
-    check('description', 'The description is required').notEmpty(),
-    check('category', 'The category is not valid').isMongoId(), // Evaluamos quie si el Id de la categoria que le pasamos es valida
-    
+    //  check('name', 'The name is required').notEmpty(),
+    // check('image', 'The image is required').notEmpty(),
+    check("price", "The price is required").notEmpty(),
+    check("description", "The description is required").notEmpty(),
+    check("category", "The category is not valid").isMongoId(), // Evaluamos quie si el Id de la categoria que le pasamos es valida
+
     // check('year', 'The year is required').notEmpty(),
     // check('make', 'The make is required').notEmpty(),
     // check('model', 'The model is required').notEmpty(),
@@ -44,20 +49,22 @@ router.post('/',[
     // check('fuel', 'The fuel is required').notEmpty(),
     // check('vin', 'The vin is required').notEmpty(),
     // check('bodySize', 'The body size is required').notEmpty(),
-    
-    
-    validateFields
-], createPost);
 
-router.put('/:id',[
+    validateFields,
+  ],
+  createPost
+);
 
+router.put(
+  "/:id",
+  [
     validateJwt,
-  //  check('name', 'The name is required').notEmpty(),
-   // check('image', 'The image is required').notEmpty(),
-    check('price', 'The price is required').notEmpty(),
-    check('description', 'The description is required').notEmpty(),
-    check('category', 'The category is not valid').isMongoId(), // Evaluamos quie si el Id de la categoria que le pasamos es valida
-    
+    //  check('name', 'The name is required').notEmpty(),
+    // check('image', 'The image is required').notEmpty(),
+    check("price", "The price is required").notEmpty(),
+    check("description", "The description is required").notEmpty(),
+    check("category", "The category is not valid").isMongoId(), // Evaluamos quie si el Id de la categoria que le pasamos es valida
+
     // check('year', 'The year is required').notEmpty(),
     // check('make', 'The make is required').notEmpty(),
     // check('model', 'The model is required').notEmpty(),
@@ -67,12 +74,12 @@ router.put('/:id',[
     // check('fuel', 'The fuel is required').notEmpty(),
     // check('vin', 'The vin is required').notEmpty(),
     // check('bodySize', 'The body size is required').notEmpty(),
-    
-    
-    validateFields
 
-], updatePost);
+    validateFields,
+  ],
+  updatePost
+);
 
-router.delete('/:id',validateJwt, deletePost);
+router.delete("/:id", validateJwt, deletePost);
 
 module.exports = router;
